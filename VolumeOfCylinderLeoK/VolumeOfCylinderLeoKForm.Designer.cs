8e41ab716c969e3b977a5755f527bd2ce83a5cae
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-04-25 12:07:52 -0400

    Initial commit

---
+++ VolumeOfCylinderLeoK/VolumeOfCylinderLeoKForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace VolumeOfCylinderLeoK
+{
+    partial class frmVolumeOfCylinderLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.lblHeightRequest = new System.Windows.Forms.Label();
+            this.lblRadiusRequest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(133, 176);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(110, 35);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(253, 48);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 1;
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(253, 106);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 20);
+            this.txtRadius.TabIndex = 2;
+            // 
+            // lblHeightRequest
+            // 
+            this.lblHeightRequest.AutoSize = true;
+            this.lblHeightRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeightRequest.Location = new System.Drawing.Point(12, 48);
+            this.lblHeightRequest.Name = "lblHeightRequest";
+            this.lblHeightRequest.Size = new System.Drawing.Size(201, 20);
+            this.lblHeightRequest.TabIndex = 3;
+            this.lblHeightRequest.Text = "Please enter the height:";
+            // 
+            // lblRadiusRequest
+            // 
+            this.lblRadiusRequest.AutoSize = true;
+            this.lblRadiusRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRadiusRequest.Location = new System.Drawing.Point(12, 106);
+            this.lblRadiusRequest.Name = "lblRadiusRequest";
+            this.lblRadiusRequest.Size = new System.Drawing.Size(200, 20);
+            this.lblRadiusRequest.TabIndex = 4;
+            this.lblRadiusRequest.Text = "Please enter the radius:";
+            // 
+            // frmVolumeOfCylinderLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 261);
+            this.Controls.Add(this.lblRadiusRequest);
+            this.Controls.Add(this.lblHeightRequest);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmVolumeOfCylinderLeoK";
+            this.Text = "Volume Of A Cylinder by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label lblHeightRequest;
+        private System.Windows.Forms.Label lblRadiusRequest;
+    }
+}
+
---
+++ VolumeOfCylinderLeoK/VolumeOfCylinderLeoKForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolumeOfCylinderLeoK
+{
+    public partial class frmVolumeOfCylinderLeoK : Form
+    {
+        public frmVolumeOfCylinderLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private double CalculateVolume(double height, double radius)
+        {
+            //variables
+            const double PI = Math.PI;
+            double volume;
+
+            //calculate the volume
+            volume = PI * Math.Pow(radius, 2) * height;
+
+            //round the volume
+            volume = Math.Round(volume, 2);
+
+            //return volume
+            return volume;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //variables
+            double userHeight;
+            double userRadius;
+            double userVolume;
+
+            //get the radius and height from the user
+            if (txtHeight.Text == "")
+            {
+                MessageBox.Show("Please enter the height.", "Volume of a Cylinder");
+            }
+            else if (txtRadius.Text == "")
+            {
+                MessageBox.Show("Please enter the radius", "Volume of a Cylinder");
+            }
+            else
+            {
+                //convert the user input to a double
+                userHeight = Convert.ToDouble(txtHeight.Text);
+                userRadius = Convert.ToDouble(txtRadius.Text);
+
+                //call the function CalculateVolume
+                userVolume = CalculateVolume(userHeight, userRadius);
+
+                //Display the Volume
+                MessageBox.Show("The volume of the cylinder is " + userVolume, "Volume of a Cylinder");
+            }
+        }
+    }
+}
